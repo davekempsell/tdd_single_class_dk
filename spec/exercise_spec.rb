627993c03a1bf04d42aa57bbc1a2f65422af8b9a
Joe McGarry <joewmcgarry@gmail.com>
2022-05-18 16:45:47 +0100

    adds more methods and tests to example and example_spec.rb

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -1,4 +1,4 @@
-require 'exercise'
+require "exercise"
 
 RSpec.describe DiaryEntry do
   it "returns the title as a string." do
@@ -8,7 +8,7 @@ RSpec.describe DiaryEntry do
 
   it "returns the contents as a string." do
     entry = DiaryEntry.new("Monday", "Example.")
-    expect(entry.contents).to eq "Example"
+    expect(entry.contents).to eq "Example."
   end
 
   it "Returns the number of words in the contents as an integer." do
@@ -16,7 +16,17 @@ RSpec.describe DiaryEntry do
     expect(entry.count_words).to eq 1
   end
 
-  it "Returns an integer representing an estimate of the reading time in minutes." do
-    entry = DiaryEntry.new("Monday", "Example.")
-    
+  it "Returns an integer representing an estimate of the reading time in 
+    minutes." do
+    entry = DiaryEntry.new("Monday", "Example. " * 10000)
+    result = entry.reading_time(100)
+    expect(result).to eq 100
+  end
+
+  it "Returns a string with part of the contents that the user could read
+      in the given number of minutes." do
+    entry = DiaryEntry.new("Monday", "Example. Another word to go here.")
+    result = entry.reading_chunk(1, 5)
+    expect(result).to eq "Example. Another word to go"
+  end
 end
